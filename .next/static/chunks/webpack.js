29fa488d839867f6a8e300ac3186b748a72506a6
Mike Zbrozek <mzbrozek@cnm.edu>
2022-09-21 18:33:29 -0600

    Starting to build a nextjs navbar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0a0b3f431e1437c4"; }
+/******/ 		__webpack_require__.h = function() { return "2db09590dc64e389"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
